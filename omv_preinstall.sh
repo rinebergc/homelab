391e8d0eb912354afb75865fd2cf06206cd0896c
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2025-01-22 15:53:35 -0500

    Update omv_preinstall.sh

--- omv_preinstall.sh
+++ omv_preinstall.sh
@@ -2,15 +2,12 @@
 
 # Pre-installation Script
 # Based on https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/preinstall
-# https://wiki.omv-extras.org/doku.php?id=omv7:raspberry_pi_install
+# See https://wiki.omv-extras.org/doku.php?id=omv7:raspberry_pi_install for more information on installing OMV
 
-# wget -O - https://raw.githubusercontent.com/rinebergc/homelab/main/omv_preinstall.sh | sudo bash
+# Run this script using the following command: wget -O - https://raw.githubusercontent.com/rinebergc/homelab/main/omv_preinstall.sh | sudo bash
 
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
-export LANG=C.UTF-8
-export LANGUAGE=C
-export LC_ALL=C.UTF-8
 
 sudo apt update && sudo apt upgrade -y --no-install-recommends
 sudo raspi-config do_net_names 0
