a0529dba6c4045e2bd27aa79c4f5eec92bd9b91f
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2025-01-22 15:49:54 -0500

    Update omv_preinstall.sh

--- omv_preinstall.sh
+++ omv_preinstall.sh
@@ -1,6 +1,27 @@
-# Pre-installation
+#!/bin/bash
+
+# Pre-installation Script
+# Based on https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/preinstall
 # https://wiki.omv-extras.org/doku.php?id=omv7:raspberry_pi_install
-sudo apt update
-sudo apt upgrade -y
-wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/preinstall | sudo bash
+
+# wget -O - https://github.com/rinebergc/homelab/edit/main/omv_preinstall.sh | sudo bash
+
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+export LANG=C.UTF-8
+export LANGUAGE=C
+export LC_ALL=C.UTF-8
+
+sudo apt update && sudo apt upgrade -y --no-install-recommends
+sudo raspi-config do_net_names 0
+
+INTERFACE="enx6c1ff7171aa4"
+MAC="6c:1f:f7:17:1a:a4"
+
+echo "Interface Name: ${INTERFACE}"
+echo "MAC Address: ${MAC}"
+echo -e "[Match]\nMACAddress=${MAC}\n[Link]\nName=${INTERFACE}" > /etc/systemd/network/10-persistent-eth0.link
+
+read -p "Press enter to reboot..."
+
 sudo reboot
