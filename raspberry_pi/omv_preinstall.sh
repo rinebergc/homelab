982adfc6d174a8d19699f7d71e79390e1460ee11
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2025-01-23 01:06:17 -0500

    Update omv_preinstall.sh

--- raspberry_pi/omv_preinstall.sh
+++ raspberry_pi/omv_preinstall.sh
@@ -4,7 +4,7 @@
 # Inspired by https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/preinstall
 # See https://wiki.omv-extras.org/doku.php?id=omv7:raspberry_pi_install for more information on installing OMV
 
-# Run this script using the following command: wget -O - https://raw.githubusercontent.com/rinebergc/homelab/refs/heads/main/omv_preinstall.sh | sudo bash
+# Run this script using the following command: wget -O - https://raw.githubusercontent.com/rinebergc/homelab/refs/heads/main/raspberry_pi/omv_preinstall.sh | sudo bash
 
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
@@ -15,7 +15,7 @@ apt update && apt upgrade -y --no-install-recommends
 
 echo "Configuring LAN..."
 # Get the name of the active connection
-ACTIVE_CONNECTION="$(nmcli -t -f NAME con show --active)"
+ACTIVE_CONNECTION="$(nmcli -t -f NAME con show --active | grep -v "lo")"
 # Get the name of the active interface
 INTERFACE="enx$(nmcli -t -g GENERAL.HWADDR,GENERAL.CONNECTION device show | grep -B 1 "${ACTIVE_CONNECTION}" | sed -n '1s/\\//g;1s/://g;1p' | tr 'A-Z' 'a-z')"
 # Configure auto-connect
@@ -25,7 +25,7 @@ if [[ "$(nmcli -t -f DEVICE con show --active)" != "${INTERFACE}" ]]; then
   raspi-config nonint do_net_names 0
 fi
 # Disable IPv6
-if [[ "$(cat /proc/sys/net/ipv6/conf/${INTERFACE}/disable_ipv6 2>/dev/null)" != "1" ]]; then
+if [[ "$(cat /proc/sys/net/ipv6/conf/${INTERFACE}/disable_ipv6 &>/dev/null)" != "1" ]]; then
   nmcli con modify "${ACTIVE_CONNECTION}" ipv6.method "disabled"
 fi
 
@@ -47,6 +47,9 @@ if ! dpkg-query -W rockpi-penta | grep -Fq "rockpi-penta"; then
   # Remove installation package
   rm rockpi-penta-0.2.2.deb
 fi
+
+# Allows users who are not logged in to run long-running services (Raspberry Pi OS Lite)
+loginctl enable-linger
 
 echo -e "\nThe system will restart in 5 seconds.\n"
 sleep 5
