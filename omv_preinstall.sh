babdd40c81294527ddf7b2f8c81b932c764a176e
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2025-01-22 20:02:58 -0500

    Update omv_preinstall.sh

--- omv_preinstall.sh
+++ omv_preinstall.sh
@@ -4,43 +4,44 @@
 # Inspired by https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/preinstall
 # See https://wiki.omv-extras.org/doku.php?id=omv7:raspberry_pi_install for more information on installing OMV
 
-# Run this script using the following command: wget -O - https://raw.githubusercontent.com/rinebergc/homelab/refs/heads/main/omv_preinstall.sh | sudo bash
+# Run this script using the following command: wget -O - https://raw.githubusercontent.com/rinebergc/homelab/refs/heads/main/omv_preinstall.sh | bash
 
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
 
 echo "Checking for available package upgrades..."
-sudo apt update && sudo apt upgrade -y --no-install-recommends # Retrieve and install available package upgrades
+apt update && apt upgrade -y --no-install-recommends # Retrieve and install available package upgrades
 
 if ! grep -Fq "ipv6.disable=1" /boot/firmware/cmdline.txt; then
   echo "Appending ipv6.disable=1 to cmdline.txt..."
-  echo " ipv6.disable=1" | sudo tee -a /boot/firmware/cmdline.txt # Disable IPv6
+  echo " ipv6.disable=1" | tee -a /boot/firmware/cmdline.txt # Disable IPv6
 fi
 
-if nmcli -t -f NAME con | grep -Fq "Wired connection 2"; then
-  echo "Configuring LAN..."
-  if ! nmcli -t -f NAME con | grep -Fq "enx6c1ff7171aa4"; then
-    sudo raspi-config nonint do_net_names 0 # Enable predictable network interface names
-  fi
-  sudo nmcli con modify Wired\ connection\ 2 connection.autoconnect true
-  sudo nmcli con modify Wired\ connection\ 2 connection.autoconnect-priority 999
-  sudo nmcli con modify Wired\ connection\ 2 connection.autoconnect-retries 0
+if [[ "$(nmcli -t -f NAME con show --active)" != "enx6c1ff7171aa4" ]]; then
+  echo "Enabling predictable network interface names..."
+  raspi-config nonint do_net_names 0 # Enable predictable network interface names
 fi
 
-if sudo systemctl is-enabled hciuart | grep -Fq "Enabled"; then
+echo "Configuring LAN..."
+ACTIVE_CONNECTION="$(nmcli -t -f NAME con show --active)"
+nmcli con modify "${ACTIVE_CONNECTION}" connection.autoconnect true
+nmcli con modify "${ACTIVE_CONNECTION}" connection.autoconnect-priority 999
+nmcli con modify "${ACTIVE_CONNECTION}" connection.autoconnect-retries 0
+
+if [[ "$(systemctl is-enabled hciuart)" != "Enabled" ]]; then
   echo "Disabling hciuart..."
-  if sudo systemctl is-active hciuart | grep -Fq "Active"; then
-    sudo systemctl stop hciuart
+  [[ "$(systemctl is-active hciuart)" != "Active" ]]; then
+    systemctl stop hciuart
   fi
-  sudo systemctl disable hciuart # Disable the service for initalizing/configuring Bluetooth modems
+  systemctl disable hciuart # Disable the service for initalizing/configuring Bluetooth modems
 fi
 
 if ! dpkg-query -W rockpi-penta | grep -Fq "rockpi-penta"; then
   echo "Installing the Penta TopHAT drivers..."
   wget https://github.com/radxa/rockpi-penta/releases/download/v0.2.2/rockpi-penta-0.2.2.deb # Download the Penta TopHAT drivers
-  sudo apt install -y ./rockpi-penta-0.2.2.deb
+  apt install -y ./rockpi-penta-0.2.2.deb
 fi
 
 echo -e "\nThe system will restart in 5 seconds.\n"
 sleep 5
-sudo systemctl --no-wall reboot
+systemctl --no-wall reboot
